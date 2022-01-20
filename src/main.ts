345e8ee6c9117dabbe1365f55dd81bf56e32d63d
Justin Sun <i@jsun969.cn>
2022-01-20 20:21:04 +0800

    feat: init `prisma` with `nestjs`

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './common/services/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
