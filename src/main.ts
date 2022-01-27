f8fc7025f03253a96d3d1a7032e91978996b8ca0
Justin Sun <i@jsun969.cn>
2022-01-27 10:55:52 +0800

    feat(config): use port in env

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
   const prismaService: PrismaService = app.get(PrismaService);
   prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
