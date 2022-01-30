db39ab44680fff480009d59e5c0553ca87b9447f
Justin Sun <i@jsun969.cn>
2022-01-31 00:13:14 +0800

    feat(auth): add websocket guard

---
+++ src/auth/guards/teacher-ws-auth.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { PrismaService } from '~/common/services/prisma.service';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class TeacherWsAuthGuard implements CanActivate {
+  constructor(
+    private readonly authService: AuthService,
+    private readonly db: PrismaService,
+  ) {}
+
+  async canActivate(context: ExecutionContext) {
+    try {
+      const data = context.switchToWs().getData();
+      const { token } = data;
+      const { userId } = await this.authService.verifyJwtWithId(token);
+      const isIdExist =
+        (await this.db.teacher.findUnique({
+          where: { id: userId },
+        })) !== null;
+      if (isIdExist) {
+        data.userId = userId;
+      }
+      return isIdExist;
+    } catch (error) {
+      return false;
+    }
+  }
+}
