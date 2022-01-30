f3619c3244a2a7ad993ae419c43e3f783abddf74
Justin Sun <i@jsun969.cn>
2022-01-30 16:15:59 +0800

    style(app/import): sort import order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AdminModule } from './admin/admin.module';
-import { TeacherModule } from './teacher/teacher.module';
 import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
 import { StudentModule } from './student/student.module';
+import { TeacherModule } from './teacher/teacher.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), AdminModule, TeacherModule, StudentModule],
