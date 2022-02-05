84b93d0765b06da4f027a3f9bca4266f8f70127c
Justin Sun <i@jsun969.cn>
2022-02-05 11:20:51 +0800

    feat(util/array): add `id` generic

--- src/common/utils/delete-array-element-by-id.util.ts
+++ src/common/utils/delete-array-element-by-id.util.ts
@@ -1,6 +1,6 @@
-export function deleteArrayElementById<T extends { id: string }>(
-  array: T[],
-  id: string,
+export function deleteArrayElementById<T1 extends { id: T2 }, T2>(
+  array: T1[],
+  id: T2,
 ) {
   const index = array.findIndex(({ id: originId }) => originId === id);
   array.splice(index, 1);
--- src/common/utils/find-array-element-by-id.util.ts
+++ src/common/utils/find-array-element-by-id.util.ts
@@ -1,6 +1,6 @@
-export function findArrayElementById<T extends { id: string }>(
-  array: T[],
-  id: string,
+export function findArrayElementById<T1 extends { id: T2 }, T2>(
+  array: T1[],
+  id: T2,
 ) {
   return array.find(({ id: originId }) => originId === id);
 }
